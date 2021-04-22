dd6488ee0c03f1e65394a906de9eaa10a9523e65
Duy V. Huynh <duy.huynh2@tiki.vn>
2021-04-22 14:49:24 +0700

    update prerequisite scripts

--- pre_env.sh
+++ pre_env.sh
@@ -1,5 +1,8 @@
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.1+cpu.html
-pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.1+cpu.html
-pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.1+cpu.html
-pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.1+cpu.html
+TORCH_VERSION=1.8.1
+CUDA_VERSION=cpu
+
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
 pip install torch-geometric
