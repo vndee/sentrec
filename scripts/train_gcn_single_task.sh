8705f281034efa7e12e0546c01eaf9920d66f92f
Duy V. Huynh <duy.huynh2@tiki.vn>
2021-04-23 16:25:41 +0700

    update training scripts

--- scripts/train_gcn_single_task.sh
+++ scripts/train_gcn_single_task.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-python train.py --device cuda:2
+python train.py --input data/Reviews.csv \
+                --device cuda:2 \
+                --learning_rate 0.001 \
+                --epoch 200 \
+                --text_feature False \
+                --multi_task False \
+                --random_seed 42
